9c347d87c99ef4ebf737abf0cd1cbf48d984ec1f
what232-star <13560250826@163.com>
2026-03-29 09:30:08 +0800

    后端的起始配置完成

---
+++ penguin-admin/src/main/java/com/penguin/PenguinApplication.java
@@ -0,0 +1,37 @@
+package com.penguin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ * git@github.com:what232-star/qie-front.git  前端仓库
+ * git@github.com:what232-star/qie-behind.git 后端仓库
+ *
+ *
+ * cd D:\Redis\Redis-x64-3.2.100
+ * redis-server.exe redis.windows.conf
+ * cd D:\404_qie\Penguin
+ *
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class PenguinApplication
+{
+    public static void main(String[] args)
+    {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(PenguinApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
---
+++ penguin-common/src/main/java/com/penguin/common/core/domain/model/RegisterBody.java
@@ -0,0 +1,11 @@
+package com.penguin.common.core.domain.model;
+
+/**
+ * 用户注册对象
+ * 
+ * @author ruoyi
+ */
+public class RegisterBody extends LoginBody
+{
+
+}
