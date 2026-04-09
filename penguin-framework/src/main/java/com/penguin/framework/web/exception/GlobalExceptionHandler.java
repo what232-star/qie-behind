028ea15f2b1ae063e9394aaf21f1910e510671f7
what232-star <13560250826@163.com>
2026-04-09 14:47:05 +0800

    1在数据库中考虑数据的完整性，把node和region,bear的外键约束改成限制约束而不是级联约束，强行控制避免出现误删大量的关联点位的情况 2.在framework模块中的全局异常控制处理类中添加一个新的处理数据完整性异常的信息处理。 3.

--- penguin-framework/src/main/java/com/penguin/framework/web/exception/GlobalExceptionHandler.java
+++ penguin-framework/src/main/java/com/penguin/framework/web/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.penguin.framework.web.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -142,4 +143,17 @@ public class GlobalExceptionHandler
     {
         return AjaxResult.error("演示模式，不允许操作");
     }
+
+
+    //数据完整性异常
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public AjaxResult handleDataIntegrityViolationException(DataIntegrityViolationException e)
+        {
+            log.error(e.getMessage(), e);
+            if(e.getMessage().contains("foreign"))
+            {
+                return AjaxResult.error("此数据已被引用，无法删除");
+            }
+            return AjaxResult.error("数据完整性异常");
+        }
 }
